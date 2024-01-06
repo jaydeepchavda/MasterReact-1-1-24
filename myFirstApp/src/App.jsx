88c54025f8c12888e8a642f64a075d1e2871e991
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-07 01:39:14 +0530

    make a new functional component commit

--- myFirstApp/src/App.jsx
+++ myFirstApp/src/App.jsx
@@ -1,35 +1,34 @@
-
-import React, { Component } from 'react';
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      monster : []
+      monster: [],
     };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monster : users}));
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monster: users }));
   }
-  render(){
-  return (
-    <>
-     <h1>Hello world</h1>
- 
+  render() {
+    return (
+      <>
+        {/* <h1>Hello world</h1> */}
 
+        <CardList name="jaydip">
+          {this.state.monster.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
 
-     <h3>create app with vite </h3> 
-     {
-      this.state.monster.map(monster => <h1>{monster.name}  </h1>)
-     }
-    
-    </>
-  )
+        {/* <h3>create app with vite </h3>  */}
+      </>
+    );
+  }
 }
-}
 
-export default App
+export default App;
---
+++ myFirstApp/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+
+export const CardList =  (props) =>{
+    console.log(props);
+    return( 
+    <div>{props.children}</div>
+    )
+}
