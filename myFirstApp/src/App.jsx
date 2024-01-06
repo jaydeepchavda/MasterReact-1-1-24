e6f7a605af9a51f6830c2443873acb03a88274a4
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-07 00:54:01 +0530

    class component commit

--- myFirstApp/src/App.jsx
+++ myFirstApp/src/App.jsx
@@ -1,13 +1,45 @@
 
+import React, { Component } from 'react';
 import './App.css'
+import First from './first'
+class App extends Component {
 
-function App() {
+  constructor() {
+    super();
+    this.state = {
+      monster : [
+        {
 
+          name:'franklin',
+          id: 'aasd1'
+        },
+        {
+          name:'dorbeman',
+          id:'aasd2'
+        },
+        {
+          name:"pristiges",
+          id:'assd3'
+        }
+        
+      ]
+    };
+  }
+  render(){
   return (
     <>
-     <h1>Hello world ,my name is Jaydip</h1> 
+     <h1>Hello world</h1>
+     {/* <First /> */}
+
+
+     <h3>create app with vite </h3> 
+     {
+      this.state.monster.map(monster => <h1>{monster.name}  </h1>)
+     }
+    
     </>
   )
+}
 }
 
 export default App
---
+++ myFirstApp/src/First.jsx
@@ -0,0 +1,9 @@
+
+function First(){
+
+    return (
+        <h1>hello from first chai</h1>
+    )
+}
+
+export default First;
