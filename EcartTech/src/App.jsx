969d190d6ae21c06186307c56ef869a0ca26dfd6
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-13 03:19:01 +0530

    counter component commit

--- EcartTech/src/App.jsx
+++ EcartTech/src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Counter from "./counter";
 import "./App.css";
 
 function App() {
-  let [Counter, setCounter] = useState(0);
-
-  const addValue = () => {
-    setCounter(Counter + 1);
-    if (Counter == 20) {
-      setCounter(20);
-    }
-  };
-
-  const removeValue = () => {
-    setCounter(Counter - 1);
-    if (Counter == 0) {
-      setCounter(0);
-    }
-  };
-
+  
   return (
     <>
-      <h1> hello world</h1>
-      
-      <h2>This is a counter project </h2>
-      <h3>Counter Value : {Counter}</h3>
-      <button onClick={addValue}>Add value +</button> <br />
-      <button onClick={removeValue}>Remove value -</button>
+    <h1> First Project in React </h1>
+    <Counter />
     </>
   );
 }
---
+++ EcartTech/src/Counter.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function Counter(){
+    let [Counter, setCounter] = useState(0);
+
+    const addValue = () => {
+      setCounter(Counter + 1);
+      if (Counter == 20) {
+        setCounter(20);
+      }
+    };
+
+    
+  
+    const removeValue = () => {
+      setCounter(Counter - 1);
+      if (Counter == 0) {
+        setCounter(0);
+      }
+    };
+  
+    return(
+        
+        <>
+      <h1> hello world</h1>
+
+      <h2>This is a counter project </h2>
+      <h3>Counter Value : {Counter}</h3>
+      <button onClick={addValue}>Add value +</button> <br />
+      <button onClick={removeValue}>Remove value -</button>
+    </>
+    )
+    
+}
+
+export default Counter;
