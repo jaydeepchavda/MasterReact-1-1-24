7037286aebf5c518aeb7411ceec02f73a983fd23
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-13 19:22:02 +0530

    tailwind css example commit

--- EcartTech/src/App.jsx
+++ EcartTech/src/App.jsx
@@ -1,12 +1,17 @@
 import Counter from "./counter";
 import "./App.css";
+import LoginTailwind from "./component-tailwind-login/login-tailwind";
 
 function App() {
   
   return (
     <>
-    <h1> First Project in React </h1>
-    <Counter />
+    {/* <h1> First Project in React </h1> */}
+    {/* <Counter /> */}
+
+    <h2 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind install</h2>
+    
+    <LoginTailwind />
     </>
   );
 }
