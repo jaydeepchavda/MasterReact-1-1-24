be71a1cc274825d716f3e4e73fa6a3c32bbcfa99
jaydeepchavda <jaydipchavda2216@gmail.com>
2024-01-27 23:42:40 +0530

    change proxy commit

--- foodApp/src/components/Body.jsx
+++ foodApp/src/components/Body.jsx
@@ -22,7 +22,7 @@ const Body = () => {
 
    const fetchData = async () => {
     const data = await fetch(
-      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING'
+      "https://corsproxy.io /?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
@@ -30,6 +30,8 @@ const Body = () => {
     // console.log(json);
     // * optional chaining
     // setListOfRestaurants(json.data.cards[2].data.data.cards);
+      console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
     setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   };
